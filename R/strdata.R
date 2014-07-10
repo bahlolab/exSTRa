f3194d3a32955ddc444fda82b1e0e30382aa9252
Rick Tankard <tankard@wehi.edu.au>
2014-07-10 18:34:20 +1000

    Created the strdata class

---
+++ R/strdata.R
@@ -0,0 +1,20 @@
+# The strdata class
+# Holds information related to STR counts as obtained from other programs
+
+library(data.table)
+library(testit)
+
+is.strdata <- function(x) inherits(x, "strdata")
+
+read.strs <- function(data, database) {
+  # Load the STR counts
+  # read.strs(data = "read_counts.txt", database = strdatabase)
+  assert("read.strs requires database to be a strdb", inherits(database, "strdb"))
+  # Read in data
+  counts <- read.delim(data.in.file)
+  return(strdata_new(counts, database))
+}
+
+strdata_new <- function(data, db) {
+  structure(list(data = data, db = db), class = c("strdata"))
+}
