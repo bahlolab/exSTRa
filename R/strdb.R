904766e3b0943a1040405aad85dfff9044d13a95
Rick Tankard <tankard@wehi.edu.au>
2014-08-14 15:30:34 +1000

    Now reading UCSC files, even if it still needs some work. Using chrom, chromStart and chromEnd instead of the STR db coordinates now.

--- R/strdb.R
+++ R/strdb.R
@@ -10,10 +10,7 @@ strdb <- function(dt, input_type = NULL) {
   # Transforms a data.frame or data.table into a strdb object
   if (!is.data.frame(dt)) stop("dt must be data.frame")
   dt <- data.table(dt)
-  #class(dt) <- c("strdb", class(dt))
-  #dt
-  #dt <- dt[!is.na(hg19.chrom) hg19.start.0 hg19.end, ]
-  dt <- dt[!(is.na(hg19.chrom) | is.na(hg19.start.0) | is.na(hg19.end))]
+  dt <- dt[!(is.na(chrom) | is.na(chromStart) | is.na(chromEnd))]
   structure(list(db = dt, input_type = input_type), class = c("strdb"))
 }
 
@@ -26,30 +23,50 @@ strdb_new <- function() {
 
 is.strdb <- function(x) inherits(x, "strdb")
 
-strdb_read <- function(file) {
+strdb_read <- function(file, ...) {
   # Open up a file to load a STR database object
   if (!is.character(file)) stop("file must be character")
   strdatabase <- switch(str_extract(file, "[^.]*$"), # note str_extract is a stringr function, can be very confusing
-    xlsx = strdb_xlsx(file),
-    strdb_text(file))
+    xlsx = strdb_xlsx(file, ...),
+    strdb_ucsc(file, ...))
   if(is.null(strdatabase)) {
     stop("Could not determine the file type")
   }
   strdatabase
 }
 
-strdb_xlsx <- function(file) {
+strdb_xlsx <- function(file, ...) {
   if (!is.character(file)) stop("file must be character")
-  data <- read.xlsx(file, 1)
+  data <- read.xlsx(file, 1, ...)
   assert("xlsx requires Disease column", ! is.null(data$Disease))
   data <- replace(data, data == "NA", NA)
   data$disease.symbol <- sub(".*\\((.*)\\).*", "\\1", data$Disease, perl = T)
+  names(data)[which(names(data) == "hg19.chrom")] <- "chrom"
+  names(data)[which(names(data) == "hg19.start.0")] <- "chromStart"
+  names(data)[which(names(data) == "hg19.end")] <- "chromEnd"
   strdb(data, "named")
 }
 
-strdb_ucsc <- function(file) {
+strdb_ucsc <- function(file, header = F, ...) {
   if (!is.character(file)) stop("file must be character")
-  stop("UCSC strdb reading not yet implemented")
+  header.names <- c("X.bin", "chrom", "chromStart", "chromEnd", "name", "period", "copyNum", "consensusSize", "perMatch", "perIndel", "score", "A", "C", "G", "T", "entropy", "sequence")
+  col.classes <- c("integer", "factor", "integer", "integer", "factor", 
+                   "integer", "numeric", "integer", "integer", "integer", "integer", 
+                   "integer", "integer", "integer", "integer", "numeric", "character"
+  )
+  data <- read.delim(file, header, colClasses = col.classes, ...)
+  if(header) {
+    assert("When reading UCSC files with a header, the header lines must be: '#bin' (converted to 'X.bin' by R), 'chrom', 'chromStart', 'chromEnd', 'name', 'period', 'copyNum', 'consensusSize', 'perMatch', 'perIndel', 'score', 'A', 'C', 'G', 'T', 'entropy', 'sequence'", 
+           identical(names(data), header.names))
+  } else {
+    assert("When reading UCSC file without a header, must have 17 columns exactly", dim(data)[2] == 17)
+    names(data) <- header.names
+  }
+  assert("The column classes of the UCSC file are not as expected. Are you sure you are supplying a UCSCS file and that the header setting is correct?", prod(sapply(data, class) == col.classes) == 1)
+  data$locus <- with(data, paste0(chrom, ":", chromStart + 1, "-", chromEnd, ":", sequence))
+  data <- data.table(data)
+  data <- data[nchar(sequence) >= 2 & nchar(sequence) <= 6]
+  strdb(data, "ucsc")
 }
 
 strdb_text <- function(file) {
