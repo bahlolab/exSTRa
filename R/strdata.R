ebb7dc95fa594080678736bd7d1803f4cc5f1e30
Rick Tankard <tankard@wehi.edu.au>
2015-05-19 15:59:43 +1000

    Added the boxplot.strdata method to plot cases vs controls in our samples

--- R/strdata.R
+++ R/strdata.R
@@ -85,4 +85,60 @@ strloci.strdata <- function(data) {
   strloci(data$db)
 }
 
+boxplot.strdata <- function(strdata, locus, ..., 
+  coverage = NULL,
+  read.length = NULL,
+  cases.known = FALSE, 
+  up.cols = c("up_00", "up_01", "up_11", "up_02", "up_12", "up_22"),
+  plot.cols = c("up_01", "up_11", "up_02", "up_12")
+) {
+  # A boxplot for the strdata class
+  
+  if(xor(is.null(coverage), is.null(read.length))) {
+    stop("Must specify either none or both of 'coverage' and 'read.length' in boxplot.strdata().")
+  }
+  locus.in <- locus
+  stc <- strdata$data[locus]
+  
+  features <- melt(stc, id.vars = c("sample", "locus", "group"), variable.name = "bin", 
+    value.name = "count", measure.vars = up.cols)
+  
+  features <- features[is.element(bin, plot.cols)]
+  features$bin <- factor(features$bin, levels = plot.cols)
+  
+  disease.info <- strdata$db$db[disease.symbol == locus.in]
+  rs.len <- with(disease.info, nchar(as.character(Repeat.sequence)))
+  
+  ylimits <- NULL
+  if(!is.null(coverage)) {
+    A1 <- with(disease.info, floor(copyNum * rs.len))
+    A2 <- ifelse(cases.known, with(disease.info, floor(rn.unst.low * rs.len)), A1)
+    R <- read.length # for convienience 
+    ex_up_11 <- coverage / 2 / R * ( (R <= A1) * (A1 - R + 1) + (R <= A2) * (A2 - R + 1) )
+    ex_up_02 <- coverage / 2 / R * ( (R >= A1 + 2) * (R - A1 - 1) + (R >= A2 + 2) * (R - A2 - 1) )
+    ex_up_01 <- coverage / 2 * ( ( (R > A1 + 1) * A1 + (R > A2 + 1) * A2) / R + (R <= A1 + 1) + (R <= A2 + 1))
+    ylimits = c(0, max(ex_up_11, ex_up_02, ex_up_01, features$count))
+  }
+  
+  boxplot(count ~ bin, features[group == "control"], boxwex = 0.5, xaxt='n', 
+    xlim = c(.7, 4.4), ylim = ylimits)
+  axis(1, at = 1:4 + 0.15, labels = levels(features$bin), xlab = "Read location")
+  with(features[group == "case"], points(as.numeric(bin) + 0.34, count, col = "red"))
+  title(with(disease.info, 
+    paste0(locus.in, " (", 
+      Location.of.repeat.within.gene, " ", Repeat.sequence, ") norm: ", floor(copyNum), 
+      " (", floor(copyNum * rs.len),"bp) , exp: ", rn.unst.low, " (", 
+      floor(rn.unst.low * rs.len), "bp)")),
+    xlab = "Read Location")
+  if(!is.null(coverage)) {
+    lines(c(0.7, 1.5), rep(ex_up_01, 2), col = "blue")
+    lines(c(1.7, 2.5), rep(ex_up_11, 2), col = "blue")
+    lines(c(2.7, 3.5), rep(ex_up_02, 2), col = "blue")
+    lines(c(3.7, 4.5), rep(ex_up_01, 2), col = "blue")
+    #TODO: adjust graph so we can see higher values
+  }
+  
+}
+
+
 
