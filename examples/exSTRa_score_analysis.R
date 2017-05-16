27a131439bdac7aa8662a9e09b3e4b6a519f0e29
Rick Tankard <tankard@wehi.edu.au>
2017-05-16 12:13:30 +1000

    ready for new data
    Former-commit-id: 70fcdc4ec4e5cc72d89ba41e8c1f8e8eb6bde088

--- examples/exSTRa_score_analysis.R
+++ examples/exSTRa_score_analysis.R
@@ -7,26 +7,32 @@ library(exSTRa)
 
 # Read score data and file with loci information
 str_score <- exstra_score_read (
-  file = "data/repeat_scores_at_known_loci_TEMP.txt", # created by Perl script (TODO: exact name)
+  file = "data/HiSeqXTen_WGS_PCR_2.txt", # created by Perl script (TODO: exact name)
   #database = "data/repeat_disorders.xlsx", # for more control, use object from exstra_db_read() instead
   database = "../disease_repeats/repeat_disorders_2017_04_26.xlsx",
-  groups.regex = c(control = "", case = "^WGSrpt_1[02]$"), # here, matches on successive patterns override previous matches # (TODO: maybe should be reversed?)
+  groups.regex = c(case = "^WGSrpt", control = "^WGSrpt_0[24]$"), # here, matches on successive patterns override previous matches # (TODO: maybe should be reversed?)
   filter.low.counts = TRUE
 )
 
-#### ---- STR score ----
+str_score
+
+## ---- Plotting STR score ----
 # Plot all loci:
 # plot(str_score)
+
+# restrict to only three interesting loci, for simplicity here:
+( str_score_three <- str_score[c("HD", "SCA6", "FRDA")] )
 
 # Plot the HD locus only:
 plot(str_score["HD"])
 
 # With custom colours:
 plot(str_score, "HD", sample_col = c("WGSrpt_10" = "red", "WGSrpt_12" = "blue"))
+# Add legend:
+# TODO
 
 # For many loci, plot to a file
 
-# Perform tests
-
+## ---- Performing tests for expansions ----
 
 
