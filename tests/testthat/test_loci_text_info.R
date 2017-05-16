b4cdfe517b4423bd5eb26b6daa01a5cff8bd5646
Rick Tankard <tankard@wehi.edu.au>
2017-05-16 16:35:31 +1000

    fixed some functions, first test kind of working, but should be made into data already in the package

--- tests/testthat/test_loci_text_info.R
+++ tests/testthat/test_loci_text_info.R
@@ -1,4 +1,17 @@
 ## After running exSTRa_score_analysis.R
+context("Text information for plots loci_text_info()")
 # 
-#TODO
-loci_text_info(str_database, "SCA1")
+#TODO: make explicit code for test
+show(getwd())
+wd0 <- getwd()
+setwd("../..")
+source("examples/exSTRa_score_analysis.R")
+setwd(wd0)
+
+expected_text <- "SCA1 (coding CAG) norm: 30 (91bp) , exp: 39 (117bp)"
+
+test_that("text info has changed", {
+  expect_equal(loci_text_info(str_score, "SCA1"), expected_text)
+  expect_equal(loci_text_info(str_score$db, "SCA1"), expected_text)
+})
+
