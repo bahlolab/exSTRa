5d551f4e0e1c2f3da1cb4e167585243438824f71
Rick Tankard <tankard@wehi.edu.au>
2017-05-25 13:36:20 +1000

    Can test without errors, for Rick anyway

--- tests/testthat/test_with_large_data.R
+++ tests/testthat/test_with_large_data.R
@@ -1,17 +1,12 @@
 ## After running exSTRa_score_analysis.R
 context("Test with a real dataset")
 # 
-#TODO: make explicit code for test
-show(getwd())
-wd0 <- getwd()
-setwd("../..")
-source("examples/exSTRa_score_analysis.R")
-setwd(wd0)
 
 expected_text <- "SCA1 (coding CAG) norm: 30 (91bp) , exp: 39 (117bp)"
 
 test_that("text info has changed", {
-  expect_equal(loci_text_info(str_score, "SCA1"), expected_text)
-  expect_equal(loci_text_info(str_score$db, "SCA1"), expected_text)
+  expect_equal(loci_text_info(exstra_wgs_pcr_2, "SCA1"), expected_text)
+  expect_equal(loci_text_info(exstra_wgs_pcr_2$db, "SCA1"), expected_text)
+  expect_equal(loci_text_info(exstra_known, "SCA1"), expected_text)
 })
 
