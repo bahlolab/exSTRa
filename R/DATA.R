58f8c65a0d1e31bf09392869f76d0f276822d7ba
Rick Tankard <tankard@wehi.edu.au>
2017-05-26 15:03:36 +1000

    Made the Excel file name better

--- R/DATA.R
+++ R/DATA.R
@@ -8,7 +8,7 @@
 
 #' @export
 # TODO: should be importing text file 
-exstra_known <- read_exstra_db(system.file("extdata", "repeat_disorders_2017_05_18_external.xlsx", package = "exSTRa"))
+exstra_known <- read_exstra_db(system.file("extdata", "exSTRa_repeat_disorders.xlsx", package = "exSTRa"))
 
 #' @export
 exstra_wgs_pcr_2 <- read_score (
