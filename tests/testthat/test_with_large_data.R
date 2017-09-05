65336d4f9c1942a17c670ae3f97ad1bb2a37cf15
Rick Tankard <tankard@wehi.edu.au>
2017-09-05 16:08:55 +0800

    Test is no longer valid for the package so removed

--- tests/testthat/test_with_large_data.R
+++ tests/testthat/test_with_large_data.R
@@ -6,7 +6,6 @@ expected_text <- "SCA1 (coding CAG) norm: 30 (91bp) , exp: 39 (117bp)"
 
 test_that("text info has changed", {
   expect_equal(loci_text_info(exstra_wgs_pcr_2, "SCA1"), expected_text)
-  expect_equal(loci_text_info(exstra_wgs_pcr_2$db, "SCA1"), expected_text)
   expect_equal(loci_text_info(exstra_known, "SCA1"), expected_text)
 })
 
