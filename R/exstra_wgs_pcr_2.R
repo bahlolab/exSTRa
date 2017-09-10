93f7797cc772e5a795eb5fc52cce758311f4d536
Rick Tankard <tankard@wehi.edu.au>
2017-09-10 17:37:10 +0800

    made exSTRa have proper data sets. Basic manual pages for these data sets too.

--- R/exstra_wgs_pcr_2.R
+++ R/exstra_wgs_pcr_2.R
@@ -1,11 +1,13 @@
-#' Known repeat expansion disorder loci.
+#' Repeat score data from cohort WGS_PCR_2.
 #'
 #' Table of known repeat expansion disorder loci.
 #'
-#' @format An exstra_db object:
+#' @format An exstra_score object:
 #' \describe{
+#'   \item{data}{A data.table repeat scores, one row for each read}
+#'   \item{samples}{A data.table of input samples}
 #'   \item{db}{A data.table of the known loci}
 #'   \item{input_type}{Indication that the loci are referred to by name (as opposed to genomic position)}
 #' }
 #' @source \url{http://www.biorxiv.org/content/early/2017/06/30/157792}
-"exstra_known"
+"exstra_wgs_pcr_2"
