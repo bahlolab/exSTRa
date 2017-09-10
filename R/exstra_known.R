b669361f8278dd83cee12672b81b674590bc56dc
Rick Tankard <tankard@wehi.edu.au>
2017-09-10 17:32:27 +0800

    Start of documentation for exstra_known.

---
+++ R/exstra_known.R
@@ -0,0 +1,12 @@
+#' Known repeat expansion disorder loci.
+#'
+#' Table of known repeat expansion disorder loci.
+#'
+#' @format An exstra_db object:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.biorxiv.org/content/early/2017/06/30/157792}
+"exstra_known"
