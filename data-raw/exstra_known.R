2dfeb15c5fbd6b7e21c0463fd31bfe21ffa7df1b
Rick Tankard <tankard@wehi.edu.au>
2017-10-09 16:21:58 +0800

    Updating the data
    Former-commit-id: a7ac3ceab0c5eb4054b675f50568fdb97fea5c6f

--- data-raw/exstra_known.R
+++ data-raw/exstra_known.R
@@ -1,2 +1,2 @@
 # Read in the known repeat expansion disorder loci dataset
-exstra_known <- read_exstra_db(system.file("extdata", "exSTRa_repeat_disorders.xlsx", package = "exSTRa"))
+exstra_known <- read_exstra_db(system.file("extdata", "repeat_expansion_disorders.txt", package = "exSTRa"))
