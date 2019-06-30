4c16f47814ce10510f83ac2548d37db9718263b5
Mark Bennett <mfbennett@users.noreply.github.com>
2019-06-30 23:10:45 +1000

    Update repeat expansion database to provide separate files for hg19 and grch37 and add new loci

--- data-raw/exstra_known.R
+++ data-raw/exstra_known.R
@@ -1,2 +1,2 @@
 # Read in the known repeat expansion disorder loci dataset
-exstra_known <- read_exstra_db(system.file("extdata", "repeat_expansion_disorders.txt", package = "exSTRa"))
+exstra_known <- read_exstra_db(system.file("extdata", "repeat_expansion_disorders_hg19.txt", package = "exSTRa"))
