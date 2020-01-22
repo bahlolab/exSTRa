c766e4a702cad6227d38c462fa61f979af580166
Rick Tankard <rick.tankard@murdoch.edu.au>
2020-01-22 17:47:36 +0800

    testing replication of tsum_test() results
    Former-commit-id: 9333f98a0b1309d6634331048670cc3ae205b154

--- tests/testthat/setup_tsum_object.R
+++ tests/testthat/setup_tsum_object.R
@@ -1,6 +1,7 @@
 context("Building tsum object")
 
 set.seed(2017)
-tsum_4 <- tsum_test(exstra_wgs_pcr_2[c("HD", "SCA6", "SCA1", "FRDA")])
+four_loci <- c("HD", "SCA6", "SCA1", "FRDA")
+tsum_4 <- tsum_test(exstra_wgs_pcr_2[four_loci])
 
 Tsum_stats <- tsum_test(exstra_wgs_pcr_2[c("HD", "SCA6")], give.pvalue = FALSE)
