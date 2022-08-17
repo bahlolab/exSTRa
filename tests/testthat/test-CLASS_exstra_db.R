c6e8ebd50b4a1399836a6406bbf6958ad03dcefd
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-17 20:19:08 +1000

    adding checks to exstra_db class
    Former-commit-id: 5b4cdc8af1bdf4c84b08b18a3177adeac6fc8809

---
+++ tests/testthat/test-CLASS_exstra_db.R
@@ -0,0 +1,41 @@
+test_that("is.exstra_db() works", {
+  expect_true(is.exstra_db(exstra_known))
+  expect_true(is.exstra_db(exstra_wgs_pcr_2))
+  expect_false(is.exstra_db(pi))
+})
+
+test_that("exstra_db_new_() works", {
+  expect_error(exstra_db_new_(5))
+  expect_true(inherits(exstra_db_new_(exstra_known$db), "exstra_db"))
+  expect_true(inherits(exstra_db_new_(setnames(copy(exstra_known$db), "locus", "disease.symbol")), "exstra_db"))
+})
+
+test_that("print.exstra_db() works", {
+  expect_output(print(exstra_known), "exstra_db object with 21 loci \\(\\$db\\) of type named")
+})
+
+test_that("loci on exstra_db works", {
+  expect_true(all(is.element(loci(exstra_known), exstra_known$db$locus)))
+  expect_true(all(is.element(exstra_known$db$locus, loci(exstra_known))))
+})
+
+
+
+# loci_text_info.exstra_d
+
+# loci_normal
+
+# loci_min_exp
+
+# `[.exstra_db`
+
+# copy.exstra_db
+
+# verify.exstra_db()
+X_verify <- copy(exstra_known)
+setkey(X_verify$db, "motif")
+test_that("verify.exstra_db() works", {
+  expect_true(verify.exstra_db(exstra_known))
+  expect_error(verify.exstra_db(X_verify))
+  expect_error(verify.exstra_db(6))
+})
