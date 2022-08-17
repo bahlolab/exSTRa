7d54ba8ba4544a06bbf130c4f3ad4f9afc7a2925
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-17 21:23:57 +1000

    testing plot_multi()

---
+++ tests/testthat/test-plot_multi.R
@@ -0,0 +1,11 @@
+test_that("plot_multi()", {
+  expect_null(
+    plot_multi(exstra_wgs_pcr_2[c("HD", "SCA6", "FRDA", "SCA1")], dir = "example_images", 
+              prefix = "HiSeqXTen_WGS_PCR_2", plot_types = c(1, 2), alpha_case = 0.2))
+})
+
+test_that("plot_multi() should work on exstra_tsum objects (does not at present", {
+  expect_error(
+    plot_multi(tsum_4, dir = "example_images_tsum4", 
+               prefix = "HiSeqXTen_WGS_PCR_2", alpha_case = 0.2))
+})
