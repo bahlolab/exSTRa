7ba4ca9a82051ffed6fde07178f7860263475b29
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-18 13:31:16 +1000

    remove remove_below_quant()

--- R/private_functions.R
+++ R/private_functions.R
@@ -1,10 +1,3 @@
-remove_below_quant <- function(loc_data, quant = 0.5) {
-  loc_data[, ord := order(prop), by = sample] [, keepme := ord > max(ord) * quant, by = sample]
-  loc_data <- loc_data[identity(keepme), ]
-  loc_data[, c("ord","keepme") := NULL]
-  loc_data
-}
-
 plot_many_str_score <- function(strscore, typename, plot_cols, loci = NULL, 
   color_only = NULL, plot_types = 1, dirbase = "images/", 
   alpha_control = 0.5, alpha_case = NULL,
