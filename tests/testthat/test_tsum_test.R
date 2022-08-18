a6b23f446897dbc51aef8b85842b03c6b93359d4
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-18 21:05:52 +1000

    tsum test snapshots and plot
    Former-commit-id: 916ff9163641be72d2280be9153a5794a2d2007b

--- tests/testthat/test_tsum_test.R
+++ tests/testthat/test_tsum_test.R
@@ -1,5 +1,9 @@
-test_that("tsum_test() may not be giving T statistics", {
+test_that("tsum_test()", {
   expect_equal(dim(Tsum_stats$stats)[1], 36L)
   expect_equal(length(unique(Tsum_stats$stats$locus)), 2L)
   expect_snapshot(print(tsum_test(exstra_wgs_pcr_2[c("HD", "SCA6", "SCA1", "FRDA")])))
+})
+
+test_that("tsum_plots", {
+  vdiffr::expect_doppelganger("HD ECDF", fig = function() plot(tsum_4["HD"]))
 })
