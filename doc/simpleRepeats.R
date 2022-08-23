ba3cb9387578584711403de2adfc641c400bc9ee
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-23 17:40:53 +1000

    Update vignettes for website
    Former-commit-id: 4b612b4ac661b4a4e645a7d8f8f2217879fbda8a

--- doc/simpleRepeats.R
+++ doc/simpleRepeats.R
@@ -1,4 +1,4 @@
-## ----setup, include = FALSE----------------------------------------------
+## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
