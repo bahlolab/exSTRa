97052e3ccabdee355e961645bc4c2c693402c067
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-23 17:00:39 +1000

    using new data path
    Former-commit-id: d11122ef47eefaedb95a5d4958be2d440c13e225

--- tests/testthat/test-read_score.R
+++ tests/testthat/test-read_score.R
@@ -2,7 +2,7 @@ test_that("read_score()", {
   expect_true(
     inherits(
       read_score(
-        file = system.file("extdata", "HiSeqXTen_WGS_PCR_2.txt", package = "exSTRa"), 
+        file = system.file("extdata", "HiSeqXTen_WGS_PCR_2.txt.gz", package = "exSTRa"), 
         database = system.file("extdata", "repeat_expansion_disorders_hg19.txt", package = "exSTRa"),  # for greater control, use object from read_exstra_db() instead
         groups.regex = c(control = "^WGSrpt_0[24]$", case = ""), # the group is the first regular expression (regex) to match
         filter.low.counts = TRUE
