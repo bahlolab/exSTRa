c369ebc79f46cef76c7cb1837faeda05a7fc3158
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-30 23:57:07 +1000

    a simple output of score
    Former-commit-id: 49afd2f36c4dc1d518bc5c4cf44888b89eb458dd

--- R/score_bam.R
+++ R/score_bam.R
@@ -9,17 +9,44 @@
 score_bam <- function(paths, database, sample_names = NULL, 
                       groups.regex = NULL, groups.samples = NULL, 
                       filter.low.counts = TRUE) {
+  if (!require("GenomicAlignments", quietly = TRUE))
+    stop("The package 'GenomicAlignments' from Bioconductor is required to run this function.")
   if (!require("Rsamtools", quietly = TRUE))
     stop("The package 'Rsamtools' from Bioconductor is required to run this function.")
   
-  bam_file <- paths[1]
+  out_list <- list()
+  out_headers <- list()
+  i <- 1
+  for(bam_file in paths) {
+    out_headers[[i]] <- scanBamHeader(bam_file)
+    out_list[[i]] <- score_bam_1(bam_file, database, filter.low.counts = filter.low.counts)
+    i <- i + 1
+  }
+  list(headers = out_headers, scores = out_list)
+}
+
+# Score a single BAM file
+score_bam_1 <- function(path, database, sample_names = NULL, 
+                        groups.regex = NULL, groups.samples = NULL, 
+                        filter.low.counts = TRUE) {
   
   which <- IRangesList(seq2=IRanges(c(894), c(910)))
   what <- c("rname", "strand", "pos", "qwidth", "seq")
   param <- ScanBamParam(which=which, what=what)
-
   
-  bam <- scanBam(bam_file, param=param)
+  bampal <- readGAlignmentsList(path, param = param, with.which_label = TRUE)
   
+  
+  count_list <- list()
+  # worry about optimization later
+  for(i in seq_along(bampal)) {
+    count_list[[i]] <- str_count(bampal[[i]]@elementMetadata$seq, "CAG")
+  }
+  str_score <- unlist(count_list)
+  
+  if(filter.low.counts) {
+    # filter_low_scores(str_score)
+  }
+  str_score
 }
 
