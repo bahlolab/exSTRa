6494f829013295d460db948ff11becfc3e636b5c
Rick Tankard <trickytank@users.noreply.github.com>
2022-09-01 20:17:48 +1000

    more exploration using new data from Bio::STR::exSTRa
    Former-commit-id: bff2c5be0b27392700c3f69edccd2d3f276f495b

--- inst/development/score_bam_testing.R
+++ inst/development/score_bam_testing.R
@@ -52,18 +52,19 @@ unlist(count_list)
 
 
 # Our test
+cluster25 <- snow::makeCluster(25)
 library(fs)
 bamfiles <- dir_ls("/stornext/Bioinf/data/Bioinformatics/SNPchipdata/MPS_samples/MCRI/AGIP/kinghorn_2017_01_17/repexp_kinghorn_2017_01_17/devel/repexp_2017_01_17_pipeline/bam_recal/",
        glob = "*.bam")
 names(bamfiles) <- str_extract(bamfiles, "(?<=bam_recal/).+(?=_bowtie2_recal)")
 
 x_overlap <- score_bam(bamfiles, exstra_known, sample_names = names(bamfiles), groups.regex = c(case = "^WGSrpt", control = ""), 
-               verbosity = 2, filter.low.counts = TRUE)
+               verbosity = 2, filter.low.counts = TRUE, cluster = cluster25)
 x_count <- score_bam(bamfiles, exstra_known, sample_names = names(bamfiles), groups.regex = c(case = "^WGSrpt", control = ""), 
-                       verbosity = 2, filter.low.counts = TRUE, method = "count")
+                       verbosity = 2, filter.low.counts = TRUE, method = "count", cluster = cluster25)
 
-tsxo <- tsum_test(x_overlap)
-tsxc <- tsum_test(x_count, B = 999, correction = "samples")
+tsxo <- tsum_test(x_overlap, correction = "samples", cluster = cluster25)
+tsxc <- tsum_test(x_count, B = 999, correction = "samples", cluster = cluster25)
 tswgs2 <- tsum_test(exstra_wgs_pcr_2)
 
 par(mfrow = c(3, 1))
@@ -112,7 +113,7 @@ x_count_parallel <- score_bam(bamfiles, exstra_known, sample_name_origin = "base
 end.time <- Sys.time()
 (time.taken_parallel <- end.time - start.time)
 
-cluster25 <- snow::makeCluster(25)
+
 start.time <- Sys.time()
 x_count_parallel <- score_bam(bamfiles, exstra_known, sample_name_origin = "basename", 
                               sample_name_remove = "_bowtie2_recal.bam$",
@@ -137,3 +138,35 @@ start.time <- Sys.time()
 tst_count_snow <- tsum_test_snow(x_count_parallel, B = 9999, cluster = cluster25, correction = "samples")
 end.time <- Sys.time()
 (time.taken_tst_count_snow_premade_cluster <- end.time - start.time)
+
+
+# getting current score data
+# Read score data and file with loci information
+str_score_wgs_pcr_2 <- read_score (
+  file = "/stornext/Bioinf/data/lab_bahlo/users/tankard.r/exstra/wgs_pcr_2/wgs_pcr_2_exSTRa_scores.txt", 
+  database = system.file("extdata", "repeat_expansion_disorders_hg19.txt", package = "exSTRa"),  # for greater control, use object from read_exstra_db() instead
+  groups.regex = c(control = "^WGSrpt_0[24]$", case = ""), # the group is the first regular expression (regex) to match
+  filter.low.counts = TRUE
+)
+
+tsxo <- tsum_test(x_overlap, correction = "samples", cluster = cluster25)
+tsxc <- tsum_test(x_count, B = 999, correction = "samples", cluster = cluster25)
+ts_wgs_pcr_2 <- tsum_test(str_score_wgs_pcr_2, correction = "samples", cluster = cluster25)
+
+par(mfrow = c(3, 1))
+plot(tsxc["HD"], main = "HD, scoring by count")
+plot(tsxo["HD"], main = "HD, scoring by overlap (R)")
+plot(ts_wgs_pcr_2["HD"], main = "HD, scoring by overlap (Perl)")
+
+p_values(tsxc, only.signif = TRUE)
+
+tsxc_bf <- copy(tsxc)
+p_values(tsxc_bf, modify = TRUE)
+tsxo_bf <- copy(tsxo)
+p_values(tsxo_bf, modify = TRUE)
+ts_wgs_pcr_2_bf <- copy(ts_wgs_pcr_2)
+p_values(ts_wgs_pcr_2_bf, modify = TRUE)
+par(mfrow = c(3, 1))
+plot(tsxc["HD"], main = "HD, scoring by count")
+plot(tsxo["HD"], main = "HD, scoring by overlap (R)")
+plot(ts_wgs_pcr_2["HD"], main = "HD, scoring by overlap (Perl)")
